22d7ec7dff9446351c1b3600fb302ff046d72eb5
yangyang19910822 <1277724434@qq.com>
2018-11-22 09:33:25 +0800

    Initial Commit

---
+++ 智慧烟感/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  智慧烟感
+//
+//  Created by Mr.yang on 2018/11/22.
+//  Copyright © 2018年 Mr.yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
